6a7007ff6c4ecfaf3eaa72bd6940c36ef9acd0a4
Andrew Chumachenko <ac@sitecore.net>
2018-01-08 13:24:16 -0800

    Committing autogenerated content

---
+++ src/Sitecore.Support.162905.200956/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.162905.200956")]
+[assembly: AssemblyProduct("Sitecore.Support.162905.200956")]
+[assembly: ComVisible(false)]
